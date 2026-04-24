9544590d621c9f1c5364c499518aa93af6ffbd7b
Bawel <saviouryin1@gmail.com>
2026-04-24 16:23:46 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
 # models.py
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
-db = SQLAlchemy()
+class Client(db.Model):
+    client_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
 class Client(db.Model):
     __tablename__ = 'clients'
